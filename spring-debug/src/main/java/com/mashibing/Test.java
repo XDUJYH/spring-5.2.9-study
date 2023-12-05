35629a1bad6a75c4b1b47190c145cbf97b909edc
Admin <823213790@qq.com>
2023-12-05 18:51:34 +0800

    first commit

--- spring-debug/src/main/java/com/mashibing/Test.java
+++ spring-debug/src/main/java/com/mashibing/Test.java
@@ -9,9 +9,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
 
     public static void main(String[] args) {
-////        MyClassPathXmlApplicationContext ac = new MyClassPathXmlApplicationContext("factoryBean.xml");
-////        MyFactoryBean bean1 = (MyFactoryBean) ac.getBean( "&myFactoryBean");
-////        System.out.println(bean1);
+        MyClassPathXmlApplicationContext ac = new MyClassPathXmlApplicationContext("factoryBean.xml");
+        MyFactoryBean bean1 = (MyFactoryBean) ac.getBean( "&myFactoryBean");
+        System.out.println(bean1);
 ////        User bean = (User) ac.getBean("myFactoryBean");
 ////        System.out.println(bean.getUsername());
 ////        User bean2 = (User) ac.getBean("myFactoryBean");
